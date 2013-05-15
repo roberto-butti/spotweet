90586f1da7f8a084a3bdf391d1a43fd682c517b2
Roberto <roberto.butti@gmail.com>
2013-05-15 12:45:47 +0200

    routes

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,12 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('/', 'MainController@index');
+Route::get('/api/lang', array('as'=> 'api_tweet_count_lang', 'uses' => 'ApiController@lang'));
+Route::get('/api/users', array('as'=> 'api_tweet_count_users', 'uses' => 'ApiController@users'));
+Route::get('/api/hashtags', array('as'=> 'api_tweet_count_hashtags', 'uses' => 'ApiController@hashtags'));
+Route::get('/api/geo', array('as'=> 'api_tweet_geo', 'uses' => 'ApiController@geo'));
+
+
+//Route::resource('tweet', 'MainController');
